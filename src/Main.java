290fe78f663639af048d603fd92ce3708af531c7
BT_2S14_01 <@email>
2024-09-18 11:00:12 -0400

    I made the outputs more pleasing to look at and rounded to the calculations to the hundredths place.

--- src/Main.java
+++ src/Main.java
@@ -19,14 +19,17 @@ public class Main {
         System.out.print("Please enter the amount of people: ");
         String userPpl = y.nextLine();
         int ppl = Integer.parseInt(userPpl);
-        double amountOfTip = (bill * tip / 100);
-        double roundAmountOfTip = Math.round((amountOfTip * 100) / 100);
-        double tipPerPpl = amountOfTip / ppl;
-        double totalBill = bill + amountOfTip;
-        double totalPerPpl = totalBill / ppl;
+        double amountOfTip = Math.round((bill * tip / 100) * 100.0) / 100.0;
+        double tipPerPpl = Math.round((amountOfTip / ppl) * 100.0) / 100.0;
+        double totalBill = Math.round((bill + amountOfTip) * 100.0) / 100.0;
+        double totalPerPpl = Math.round((totalBill / ppl) * 100.0) / 100.0;
         System.out.println("------------------------------");
         System.out.println();
-        System.out.println(roundAmountOfTip);
-        System.out.println(amountOfTip);
+        System.out.println("------------------------------");
+        System.out.println("Total tip amount: $" + amountOfTip);
+        System.out.println("Total bill cost: $" + totalBill);
+        System.out.println("Tip per person: $" + tipPerPpl);
+        System.out.println("Total bill per person: $" + totalPerPpl);
+        System.out.println("------------------------------");
     }
 }
