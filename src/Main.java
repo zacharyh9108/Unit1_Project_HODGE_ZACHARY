ea0d34ecaa172760c83767bb03e84f8fee574cfb
Zacha <@email.com>
2024-09-24 22:21:07 -0400

    I implemented comments, spaced out sections of my code, and made sure outputs are working correctly.

--- src/Main.java
+++ src/Main.java
@@ -7,22 +7,30 @@ public class Main {
         System.out.println("|----------------------------------------|");
         System.out.println();
         System.out.println("------------------------------");
+
+        //Gathering user inputs for calculations
         Scanner s = new Scanner(System.in);
         System.out.print("Please enter your bill($): ");
         String userBill = s.nextLine();
         double bill = Double.parseDouble(userBill);
+
         Scanner x = new Scanner(System.in);
         System.out.print("Please enter your tip percent: ");
         String userTip = x.nextLine();
         double tip = Double.parseDouble(userTip);
+
         Scanner y = new Scanner(System.in);
         System.out.print("Please enter the amount of people: ");
         String userPpl = y.nextLine();
         int ppl = Integer.parseInt(userPpl);
-        double amountOfTip = Math.round((bill * tip / 100) * 100.0) / 100.0;
-        double tipPerPpl = Math.round((amountOfTip / ppl) * 100.0) / 100.0;
-        double totalBill = Math.round((bill + amountOfTip) * 100.0) / 100.0;
-        double totalPerPpl = Math.round((totalBill / ppl) * 100.0) / 100.0;
+
+        //Calculating user inputs inside a variable to not clutter the display statements
+        double amountOfTip = Math.round((bill * tip / 100) * 100.00) / 100.00;
+        double tipPerPpl = Math.round((amountOfTip / ppl) * 100.00) / 100.00;
+        double totalBill = Math.round((bill + amountOfTip) * 100.00) / 100.00;
+        double totalPerPpl = Math.round((totalBill / ppl) * 100.00) / 100.00;
+
+        //Displaying the calculations
         System.out.println("------------------------------");
         System.out.println();
         System.out.println("------------------------------");
